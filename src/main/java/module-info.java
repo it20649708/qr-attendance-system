dfb1ace6808cf052510e1a4757b86f83617c47c4
Naduni Samarawickrama <88743302+it20649708@users.noreply.github.com>
2023-09-16 15:14:15 +0530

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.sys2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.example.sys2 to javafx.fxml;
-    exports com.example.sys2;
-}
